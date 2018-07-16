8cafc9c9d1631c289f4fdebd7e31b96c04c97c3e
Kuba <kuba.bauman@gmail.com>
2018-07-17 00:04:00 +0200

    RequestMapping

--- src/main/java/controller/TaskController.java
+++ src/main/java/controller/TaskController.java
@@ -32,7 +32,7 @@ public class TaskController {
         return new TaskDto(1L, "Edited test title", "Test content");
     }
 
-    @RequestMapping(method = RequestMethod.PUT)
+    @RequestMapping(method = RequestMethod.PUT, value = "createTask")
     public void createTask(TaskDto task) {
 
     }
